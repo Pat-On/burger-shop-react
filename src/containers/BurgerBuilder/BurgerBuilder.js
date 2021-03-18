cf05b925e3bada2352b3213d93d8e5bc788a4e45
PatOn <mahomed2278@gmail.com>
2021-03-18 22:58:09 +0000

    wow

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Aux from "../../hoc/Auxillary/Auxillary";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 
 const INGREDIENTS_PRICES = {
   salad: 0.5,
@@ -90,7 +91,9 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-        <Modal />
+        <Modal>
+          <OrderSummary ingredients={this.state.ingredients} />
+        </Modal>
         <Burger ingredients={this.state.ingredients} />
 
         <BuildControls
