fb578cfa8fbc5c5ea82a3e956a4f3bd6c8f06eb7
PatOn <mahomed2278@gmail.com>
2021-03-18 11:06:28 +0000

    Burger Component - hard coded

--- src/components/Burger/BurgerIngredients/BurgerIngredients.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -1,5 +1,5 @@
 import React from "react";
-import classes from "./BurgerIngredient.css";
+import classes from "./BurgerIngredients.module.css";
 import PropTypes from "prop-types";
 
 const burgerIngredient = (props) => {
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 
 import Aux from "../../hoc/Auxillary/Auxillary";
-
+import Burger from "../../components/Burger/Burger";
 class BurgerBuilder extends Component {
   render() {
     return (
       <Aux>
+        <Burger />
         <div>Burger</div>
         <div>Build Controls</div>
       </Aux>
