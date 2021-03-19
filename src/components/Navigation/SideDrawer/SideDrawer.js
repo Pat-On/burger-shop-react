fe343cc5a8d5165f05792fac4abdac4972831afa
PatOn <mahomed2278@gmail.com>
2021-03-19 13:14:03 +0000

    side drawer - mobile solution

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import classes from "./SideDrawer.module.css";
+
+const sideDrawer = (props) => {
+  //... conditionally attaching the different css classes
+
+  return (
+    <div className={classes.SideDrawer}>
+      <Logo />
+      <nav>
+        <NavigationItems />
+      </nav>
+    </div>
+  );
+};
+
+export default sideDrawer;
