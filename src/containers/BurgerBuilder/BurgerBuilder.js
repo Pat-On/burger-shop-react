85b3440ad26407cfbda4d5658bc39416053f79b6
PatOn <mahomed2278@gmail.com>
2021-03-20 09:21:01 +0000

    error handling in the component by axios instance

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,9 @@ import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import Spinner from "../../components/UI/Spinnner/Spinner";
+
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
+
 import axios from "../../axios-order";
 
 const INGREDIENTS_PRICES = {
@@ -165,4 +168,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
