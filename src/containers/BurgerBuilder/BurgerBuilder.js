2ac31d74dd8f4eabcd5a8d218bc4124be567b34a
PatOn <mahomed2278@gmail.com>
2021-03-20 07:58:00 +0000

    post req

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-order";
 
 const INGREDIENTS_PRICES = {
   salad: 0.5,
@@ -89,8 +90,28 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("You continue!");
+    //alert("You continue!");
+    const order = {
+      ingredients: this.state.ingredients,
+      //recalculate prices on server because user my try to "play"
+      price: this.state.totalPrice,
+      customer: {
+        name: "Patryk ",
+        address: {
+          street: "Teststreet 1",
+          zipCode: "4152",
+          country: "uk",
+        },
+        email: "paton@gma.com",
+      },
+      deliverMethod: "fastest",
+    };
+    axios
+      .post("/orders.json", order)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
   };
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients,
