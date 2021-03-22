38b023e947b437217986883a8c417ca32de31e32
PatOn <mahomed2278@gmail.com>
2021-03-22 19:07:38 +0000

    submision and passing

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -6,25 +6,28 @@ import ContactData from "../../containers/Checkout/ContactData/ContactData";
 
 class Checkout extends Component {
   state = {
-    ingredients: {
-      salad: 1,
-      meat: 1,
-      cheese: 1,
-      bacon: 1,
-    },
+    ingredients: null,
+    price: 0,
   };
   // we have it all in componentDidMount not in componentDidUpdate so
   // the last render is going to stay - burger - it is not going to overwrite because set was set up
   // componentDidMount is not going to run because of that we added the new element
 
-  componentDidMount() {
+  componentWillMount() {
     const query = new URLSearchParams(this.props.location.search);
     const ingredients = {};
+    let price = 0;
     for (let param of query.entries()) {
-      // ["item", quantity]
-      ingredients[param[0]] = +param[1];
+      if (param[0] === "price") {
+        price = param[1];
+        console.log(price);
+      } else {
+        // ["item", quantity]
+
+        ingredients[param[0]] = +param[1];
+      }
     }
-    this.setState({ ingredients: ingredients });
+    this.setState({ ingredients: ingredients, totalPrice: price });
   }
 
   checkoutCancelledHandler = () => {
@@ -46,7 +49,17 @@ class Checkout extends Component {
         />
         <Route
           path={this.props.match.path + "/contact-data"}
-          component={ContactData}
+          // component={ContactData}
+          //manual rendering the <ContactData /> so basically
+          // we can just pass something to it
+          render={(props) => (
+            <ContactData
+              price={this.state.price}
+              ingredients={this.state.ingredients}
+              {...props} //it will include the history object so the push inside th
+              //contact data should work as well
+            />
+          )}
         />
       </div>
     );
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
+import axios from "../../../axios-order";
+// import axios from "axios";
 
+import Spinner from "../../../components/UI/Spinnner/Spinner";
 import Button from "../../../components/UI/Button/Button";
 import classes from "./ContactData.module.css";
 
@@ -11,38 +14,85 @@ class ContactData extends Component {
       street: "",
       postalCode: "",
     },
+    loading: false,
   };
+
+  orderHandler = (e) => {
+    e.preventDefault();
+    this.setState({ loading: true });
+    //alert("You continue!");
+    const order = {
+      ingredients: this.props.ingredients,
+      //recalculate prices on server because user my try to "play"
+      price: this.props.price,
+      customer: {
+        name: "Patryk ",
+        address: {
+          street: "Teststreet 1",
+          zipCode: "4152",
+          country: "uk",
+        },
+        email: "paton@gma.com",
+      },
+      deliverMethod: "fastest",
+    };
+    console.log(order);
+    axios
+      .post("orders.json", order)
+      .then((response) => {
+        console.log(response);
+        this.setState({ loading: false });
+        this.props.history.push("/");
+        return response;
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({ loading: false });
+      });
+
+    console.log(this.props.ingredients);
+  };
+
   render() {
+    let form = (
+      <form>
+        <input
+          className={classes.Input}
+          type="text"
+          name="name"
+          placeholder="Your Name"
+        />
+        <input
+          className={classes.Input}
+          type="text"
+          name="email"
+          placeholder="Your email"
+        />
+        <input
+          className={classes.Input}
+          type="text"
+          name="street"
+          placeholder="Street"
+        />
+        <input
+          className={classes.Input}
+          type="text"
+          name="postal"
+          placeholder="Postal Code"
+        />
+        <Button btnType="Success" clicked={this.orderHandler}>
+          ORDER
+        </Button>
+      </form>
+    );
+    if (this.state.loading) {
+      form = <Spinner />;
+    }
+
     return (
       <div className={classes.ContactData}>
         <h4>Enter your Contact Data</h4>
-        <form>
-          <input
-            className={classes.Input}
-            type="text"
-            name="name"
-            placeholder="Your Name"
-          />
-          <input
-            className={classes.Input}
-            type="text"
-            name="email"
-            placeholder="Your email"
-          />
-          <input
-            className={classes.Input}
-            type="text"
-            name="street"
-            placeholder="Street"
-          />
-          <input
-            className={classes.Input}
-            type="text"
-            name="postal"
-            placeholder="Postal Code"
-          />
-          <Button btnType="Success">ORDER </Button>
-        </form>
+        {form}
       </div>
     );
   }
