b0988bdcc6645e46f53560a8218223397b3ec784
PatOn <mahomed2278@gmail.com>
2021-04-01 21:54:48 +0100

    auto sign in

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -4,7 +4,12 @@
 import { takeEvery } from "redux-saga/effects";
 
 import * as actionTypes from "../actions/actionTypes";
-import { logoutSaga, checkAuthTimeoutSaga, authUserSaga } from "./auth";
+import {
+  logoutSaga,
+  checkAuthTimeoutSaga,
+  authUserSaga,
+  authCheckStateSaga,
+} from "./auth";
 
 export function* watchAuth() {
   //by this we are setting the listener to the action type and by this
@@ -12,4 +17,5 @@ export function* watchAuth() {
   yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
   yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
   yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
+  yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
 }
